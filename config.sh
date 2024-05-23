e697e8ab6d70f0a70e02428e7ecf43d651ca84e8
coder-eb <ebranbright@gmail.com>
2024-05-23 20:41:14 +0530

    Initial commit

---
+++ config.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+SCRIPT=$(realpath "$0")
+SOURCE_DIR=$(dirname "$SCRIPT")
+TARGET_DIR="$HOME/testing"
+
+DOTFILES=(.zshrc files)
+
+#Remove old dotfiles and replace them
+for dotfile in $(echo ${DOTFILES[*]});
+do
+        TARGET_FILE=$TARGET_DIR/$dotfile
+        SOURCE_FILE=$SOURCE_DIR/$dotfile
+	
+        # Removes old file
+        rm -rf $(echo $TARGET_FILE)
+        # echo $(echo $SOURCE_FILE) $(echo $TARGET_FILE) #for debugging
+        # Creates symlink between source and target file
+        ln -s $SOURCE_FILE $TARGET_FILE
+done
