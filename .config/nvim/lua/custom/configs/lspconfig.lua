41458adea7a734b8d9ff88080044a41d3c386b3a
coder-eb <ebranbright@gmail.com>
2025-03-08 22:17:32 +0530

    feat: add lsp for golang

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,7 +1,8 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
 
 -- if you just want default config for the servers then put them in a table
 local servers = { "html", "cssls", "tsserver", "clangd", "pyright" }
@@ -12,3 +13,20 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls"},
+  filetypes = {"go", "gomod", "gowork", "gotmpl"},
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+    },
+  },
+}
